cab58c988512a18f316129a7cdc283c6d1149c6a
HarryKinchin <h.j.kinchin@gmail.com>
2024-08-15 13:21:43 +0000

    Created login page outline

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 app = Flask(__name__)
   
 @app.route('/')       
 def login(): 
     return render_template("login_page.html")
+
 
 if __name__=='__main__': 
    app.run() 

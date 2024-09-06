7dcfc035d7b383d094cf8874a7fa3a27a5f6b85a
HarryKinchin <h.j.kinchin@gmail.com>
2024-09-06 13:41:22 +0000

    More stuff happened

--- main_webloader.py
+++ main_webloader.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
   
 @app.route('/')       
 def login(): 
-    return render_template("login_page.html")
+    return render_template("main_page.html")
 
 def main():
     return render_template("main_page.html")

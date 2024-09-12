4bb9af8d52a8f13c1699f6f2d0b86f5d930b383d
HarryKinchin <h.j.kinchin@gmail.com>
2024-09-12 09:29:17 +0000

    changes occured

--- database.py
+++ database.py
@@ -3,5 +3,5 @@ import sqlite3
 connection = sqlite3.connect("storage.db")
 cursor = connection.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS loginInfo(username TEXT PRIMARY KEY, password TEXT)""")
-cursor.execute(f"""INSERT INTO loginInfo(username, password) VALUES('{uname}', 'Ism311lik3b4ll5')""")
+
 connection.commit()
--- main_webloader.py
+++ main_webloader.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, send_from_directory
 app = Flask(__name__)
   
-@app.route('/')       
+@app.route('/login')       
 def login(): 
-    return render_template("main_page.html")
+    return render_template("login_page.html")
 
+@app.route('/')
 def main():
     return render_template("main_page.html")
 

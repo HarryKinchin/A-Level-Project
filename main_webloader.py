2febf2ba284be43d1a6f25514d97e19861b839a3
HarryKinchin <h.j.kinchin@gmail.com>
2024-09-16 18:42:00 +0000

    Edited the code

--- main_webloader.py
+++ main_webloader.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, send_from_directory, make_response
 app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template("main_page.html")
   
 @app.route('/login')       
 def login(): 
     return render_template("login_page.html")
 
-@app.route('/')
-def main():
-    return render_template("main_page.html")
+@app.route('/account')
+def account():
+    return render_template("account_page.html")
 
 if __name__=='__main__': 
    app.run(debug=True)

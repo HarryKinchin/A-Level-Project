b4b02b895f046bf6af50cb38b5554824cdfe4963
HarryKinchin <h.j.kinchin@gmail.com>
2024-10-31 15:58:53 +0000

    Actually finished databse design

--- temp.py
+++ temp.py
@@ -4,14 +4,3 @@ from argon2 import PasswordHasher
 connection = sqlite3.connect("storage.db")
 connection.execute("PRAGMA foreign_keys = 1")
 cursor = connection.cursor()
-
-cursor.execute("""CREATE TABLE IF NOT EXISTS questions
-                            (questionID INT PRIMARY KEY,
-                            subjectID INT,
-                            topicID INT,
-                            question TEXT,
-                            answer TEXT,
-                            FOREIGN KEY (subjectID) REFERENCES subjects (subjectID),
-                            FOREIGN KEY (topicID) REFERENCES topics (topicID))""")
-
-connection.commit()
